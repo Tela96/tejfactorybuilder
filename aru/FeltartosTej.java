0c8c44d733d87489546c78721b3299ae3051df1c
Telakos <tel.akos.2@gmail.com>
2017-02-14 14:27:03 +0100

    task 3.0 done

---
+++ aru/FeltartosTej.java
@@ -0,0 +1,14 @@
+package aru;
+
+import java.util.Date;
+
+/**
+ * Created by akos on 2017.02.14..
+ */
+public class FeltartosTej extends Tej
+{
+    public FeltartosTej(Long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom, long ar)
+    {
+        super(vonalKod, urtartalom, gyarto, szavatossagiIdo, zsirtartalom, ar);
+    }
+}
---
+++ aru/TartosTej.java
@@ -0,0 +1,15 @@
+package aru;
+
+import java.util.Date;
+
+/**
+ * Created by akos on 2017.02.14..
+ */
+public class TartosTej extends Tej
+{
+
+    public TartosTej(Long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom, long ar)
+    {
+        super(vonalKod, urtartalom, gyarto, szavatossagiIdo, zsirtartalom, ar);
+    }
+}
