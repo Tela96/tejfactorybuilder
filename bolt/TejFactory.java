c4292e03a7072018fcb6768c837b93b143313572
Telakos <tel.akos.2@gmail.com>
2017-02-16 11:31:04 +0100

    updated some files according to the correction email

--- bolt/TejFactory.java
+++ bolt/TejFactory.java
@@ -8,16 +8,12 @@ import java.util.Date;
 
 public class TejFactory
 {
-    public Tej getTej(String tejType, Long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom, long ar)
+    public Tej ujTartosTej(Long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom, long ar)
     {
-        if (tejType.equals("tartós"))
-        {
-            return new TartosTej(vonalKod, urtartalom, gyarto, szavatossagiIdo, zsirtartalom, ar);
-        }
-        else if (tejType.equals("féltartós"))
-        {
-            return new FeltartosTej(vonalKod, urtartalom, gyarto, szavatossagiIdo, zsirtartalom, ar);
-        }
-        else return null;
+        return new TartosTej(vonalKod, urtartalom, gyarto, szavatossagiIdo, zsirtartalom, ar);
+    }
+    public Tej ujFeltartosTej(Long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom, long ar)
+    {
+        return new FeltartosTej(vonalKod, urtartalom, gyarto, szavatossagiIdo, zsirtartalom, ar);
     }
 }
--- bolt/aru/Sajt.java
+++ bolt/aru/Sajt.java
@@ -4,9 +4,21 @@ import java.util.Date;
 
 public class Sajt extends Elelmiszer
 {
-
-    public Sajt(Long vonalKod, String gyarto, Date szavatossagiIdo)
+    protected double suly;
+    protected double zsirtartalom;
+    public Sajt(Long vonalKod, String gyarto, Date szavatossagiIdo, double suly, double zsirtartalom)
     {
         super(vonalKod, gyarto, szavatossagiIdo);
+        this.suly = suly;
+        this.zsirtartalom = zsirtartalom;
+    }
+
+    public double getSuly()
+    {
+        return suly;
+    }
+    public double getZsirtartalom()
+    {
+        return zsirtartalom;
     }
 }
